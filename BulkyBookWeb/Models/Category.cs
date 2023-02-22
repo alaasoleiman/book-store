2be38d3ad1a47ff508a88e066a248cd68f634918
alaasoleiman <alaa.s@facila.io>
2023-02-22 13:07:58 +0200

    Add project files.

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models
+{
+    public class Category
+    {
+        // write prop + tab to create a property (shortcut) 
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int Name { get; set; }
+        public int DisplayOrder { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+    }
+}
